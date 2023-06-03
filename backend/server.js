8ac81a188491cca3f2b6383424a312b7dab78fd3
Mason McCabe <mason.mccabe2015@gmail.com>
2023-06-03 12:01:13 -0400

    small update

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT ?? 8000;
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 // Connect to the database
 connectDB();
@@ -25,6 +26,19 @@ app.use("/api/users", require("./routes/userRouter"));
 app.use("/api/orders", require("./routes/orderRouter"));
 app.use("/api/jobs", require("./routes/jobRouter"));
 app.use("/api/operations", require("./routes/operationsRouter"));
+
+// Serve Frontend in production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+	app.get("*", (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname, "../", "frontend", "build", "index.html")
+		)
+	);
+} else {
+	app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 // Setup app listen on port
 app.listen(port, () => {
